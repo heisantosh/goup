0cd129251d978d667e4560bd74f3d61ed2dbf973
Santosh Heigrujam <santosh.hei@gmail.com>
2020-10-08 12:00:59 +0530

    Use github.com/esiqveland/notify for notification

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/heisantosh/goup
 
 go 1.15
 
-require github.com/godbus/dbus v4.1.0+incompatible
+require (
+	github.com/esiqveland/notify v0.9.1
+	github.com/godbus/dbus/v5 v5.0.3
+)
--- version.go
+++ version.go
@@ -4,6 +4,9 @@ import (
 	"log"
 	"runtime"
 	"time"
+
+	"github.com/esiqveland/notify"
+	"github.com/godbus/dbus/v5"
 )
 
 type Goversion struct {
@@ -12,30 +15,106 @@ type Goversion struct {
 
 type Goversions []Goversion
 
+type Notifier struct {
+	conn     *dbus.Conn
+	notifier notify.Notifier
+	notifID  uint32
+	version  string
+}
+
 const (
 	_versionURL           = "https://golang.org/dl/?mode=json"
 	_dlURL                = "https://storage.googleapis.com/golang/"
 	_checkIntervalMinutes = 3600 * time.Minute
+	_expireMils           = 5000
+	_appName              = "goup"
+	_appIcon              = "applications-accessories"
+	_actionUpgrade        = "upgrade"
+	_labelUpgrade         = "Upgrade"
+	_actionCancel         = "cancel"
+	_labelCancel          = "Cancel"
 )
 
+func NewNotifier() *Notifier {
+	n := Notifier{
+		conn: getDbusConn(),
+	}
+
+	notifier, err := notify.New(n.conn, notify.WithOnAction(n.onAction))
+	if err != nil {
+		log.Fatalln("error creating notifier:", err)
+	}
+	n.notifier = notifier
+
+	return &n
+}
+
+func (n *Notifier) Close() {
+	n.notifier.Close()
+	n.conn.Close()
+}
+
+func (n *Notifier) Notify(version string) {
+	notif := notify.Notification{
+		AppName:    _appName,
+		ReplacesID: n.notifID,
+		AppIcon:    _appIcon,
+		Summary:    "Go version " + version + " available!",
+		Body:       "Upgrade Go version?",
+		Actions: []string{
+			_actionCancel, _labelCancel,
+			_actionUpgrade, _labelUpgrade,
+		},
+		Hints:         map[string]dbus.Variant{},
+		ExpireTimeout: int32(_expireMils),
+	}
+
+	ID, err := n.notifier.SendNotification(notif)
+	if err != nil {
+		log.Fatalln("error sending notification:", err)
+	}
+	n.notifID = ID
+}
+
+func (n *Notifier) onAction(action *notify.ActionInvokedSignal) {
+	log.Println("action invoked:", action.ID, action.ActionKey)
+	switch action.ActionKey {
+	case _actionUpgrade:
+		log.Println("upgrading to Go version:", n.version)
+		download(n.version)
+	}
+}
+
 func main() {
+	notifier := NewNotifier()
+	defer notifier.Close()
+
 	for {
 		curr, latest := getCurrentVersion(), getLatestVersion()
 		if curr != latest {
-			// Send notification
-			// If upgrade agreed, download and upgrade
-			notif := NewNotification("Go", "Upgrade to new version!")
-			notif.AppIcon = "network-wireless"
-			if _, err := notif.Show(); err != nil {
-				log.Println("error showing notification:", err)
-			}
-
-			download(latest)
+			notifier.Notify(latest)
 		}
 
-		return
-		// time.Sleep(_checkIntervalMinutes)
+		time.Sleep(5 * time.Second)
+
+		//return
 	}
+}
+
+func getDbusConn() *dbus.Conn {
+	conn, err := dbus.SessionBusPrivate()
+	if err != nil {
+		log.Fatalln("unable to get dbus connection:", err)
+	}
+	if err = conn.Auth(nil); err != nil {
+		log.Fatalln("unable to do dbus connection auth:", err)
+	}
+
+	if err = conn.Hello(); err != nil {
+		log.Fatalln("unable to get say hello to dbus:", err)
+	}
+
+	return conn
 }
 
 func download(version string) (string, error) {
